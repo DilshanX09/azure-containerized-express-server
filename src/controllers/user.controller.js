5164bf5994ea0163f2ac955bbbc41b06aaadc6fc
Chamod Dilshan <chamoddilshan834@gmail.com>
2026-02-10 22:12:45 +0530

    feat: implement user management API with CRUD operations

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,129 @@
+const userService = require('../services/user.service');
+
+exports.createUser = async (req, res) => {
+
+  if (!req.body) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Request body is required, please provide user details.' });
+  }
+
+  const { firstName, lastName, email, mobile, password } = req.body;
+
+  if (!firstName || !lastName || !email || !mobile || !password) {
+    return res
+      .status(400)
+      .json({
+        success: false,
+        message: 'All fields are required, please provide firstName, lastName, email, mobile, and password.'
+      });
+  }
+
+  let isEmailExist = await userService.findByUserEmail(email);
+
+  if (isEmailExist) {
+    return res
+      .status(400)
+      .json({
+        success: false,
+        message: 'Email already exists, please use a different email.'
+      });
+  }
+
+  const createdUser = await userService
+    .createUser({ firstName, lastName, email, mobile, password });
+
+  if (createdUser) {
+    return res
+      .status(201)
+      .json({
+        success: true,
+        data: createdUser, message: 'User created successfully.'
+      });
+  }
+
+  return res
+    .json({ success: false, message: 'Failed to create user.' });
+}
+
+exports.getUserById = async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'User ID is required.' });
+  }
+  const user = await userService.getUserById(id);
+  if (!user) {
+    return res
+      .status(404)
+      .json({ success: false, message: 'User not found.' });
+  }
+  return res
+    .status(200)
+    .json({
+      success: true,
+      data: user, message: 'User retrieved successfully.'
+    });
+}
+
+exports.deleteUserById = async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'User ID is required.' });
+  }
+
+  const isFound = await userService.getUserById(id);
+
+  if (isFound !== null) {
+    const deletedUser = await userService.deleteUserById(id);
+    if (!deletedUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'User not found.' });
+    }
+    return res
+      .status(200)
+      .json({ success: true, data: deletedUser, message: 'User deleted successfully.' });
+  }
+  return res
+    .status(404)
+    .json({ success: false, message: 'User not found.' });
+}
+
+exports.updateUserById = async (req, res) => {
+  const { id, firstName, lastName, email, mobile } = req.body;
+  if (!id) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'User ID is required.' });
+  }
+
+  const isFound = await userService.getUserById(id);
+
+  if (isFound !== null) {
+    const updatedUser = await userService.updateUserById({ id, firstName, lastName, email, mobile });
+    if (!updatedUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'User not found.' });
+    }
+    return res
+      .status(200)
+      .json({ success: true, data: updatedUser, message: 'User updated successfully.' });
+  } else {
+    return res
+      .status(404)
+      .json({ success: false, message: 'User not found.' });
+  }
+}
+
+exports.getAllUsers = async (req, res) => {
+  const users = await userService.getAllUsers();
+  return res
+    .status(200)
+    .json({ success: true, data: users, message: 'Users retrieved successfully.' });
+}
---
+++ src/services/user.service.js
@@ -0,0 +1,120 @@
+const prisma = require("../libs/prisma");
+
+/**
+ * Finds a user by their email address.
+ * @param {string} email - The email address of the user to find.
+ * @return {boolean} Returns true if a user with the specified email exists, otherwise false.
+ * 
+*/
+exports.findByUserEmail = async (email) => {
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (user) return true;
+  return false;
+}
+
+/**
+ * Creates a new user in the database.
+ * @param {Object} user - An object containing the user's details (firstName, lastName, email, mobile).
+ * @return {Object} Returns an object containing the created user's details (id, firstName, lastName, email, mobile, createdAt).
+*/
+exports.createUser = async (user) => {
+  if (user) {
+    const createdUser = await prisma.user.create({ data: user });
+    return {
+      id: createdUser.id,
+      firstName: createdUser.firstName,
+      lastName: createdUser.lastName,
+      email: createdUser.email,
+      mobile: createdUser.mobile,
+      createdAt: createdUser.createdAt,
+    };
+  }
+}
+
+/**
+ * Retrieves a user from the database by their unique identifier.
+ * @param {number} userId - The unique identifier of the user to retrieve.
+ * @return {Object|null} Returns an object containing the user's 
+ *     details (id, firstName, lastName, email, mobile, createdAt) if found, otherwise null.
+*/
+exports.getUserById = async (userId) => {
+  if (!userId) return;
+  const user = await prisma.user.findUnique({ where: { id: parseInt(userId) } });
+  if (user) {
+    return {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      mobile: user.mobile,
+      createdAt: user.createdAt,
+    }
+  } else {
+    return null;
+  }
+}
+
+/**
+ * Deletes a user from the database by their unique identifier.
+ * @param {number} userId - The unique identifier of the user to delete.
+ *       deleted user's details (id, firstName, lastName, email, mobile, createdAt) if deletion was successful, otherwise null.
+*/
+exports.deleteUserById = async (userId) => {
+  if (!userId) return;
+  const deletedUser = await prisma.user.delete({ where: { id: parseInt(userId) } });
+  if (!deletedUser) return null;
+
+  return {
+    id: deletedUser.id,
+    firstName: deletedUser.firstName,
+    lastName: deletedUser.lastName,
+    email: deletedUser.email,
+    mobile: deletedUser.mobile,
+    createdAt: deletedUser.createdAt,
+  }
+}
+
+/**
+ * Updates a user's details in the database by their unique identifier.
+ * @param {Object} user - An object containing the user's updated details (id, firstName, lastName, email, mobile);
+ * @return {Object|null} Returns an object containing the updated user's details (id, firstName, lastName, email, mobile, createdAt) if update was successful, otherwise null.
+*/
+exports.updateUserById = async (user) => {
+  if (!user || !user.id) return;
+  const updatedUser = await prisma.user.update({
+    where: { id: parseInt(user.id) },
+    data: {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      mobile: user.mobile,
+    }
+  })
+
+  if (!updatedUser) return null;
+
+  return {
+    id: updatedUser?.id,
+    firstName: updatedUser?.firstName,
+    lastName: updatedUser?.lastName,
+    email: updatedUser?.email,
+    mobile: updatedUser?.mobile,
+    createdAt: updatedUser?.createdAt,
+  }
+}
+
+/**
+ * Retrieves all users from the database.
+ * @return {Array} Returns an array of objects, each containing a user's details (id, firstName, lastName, email, mobile, createdAt).
+*/
+exports.getAllUsers = async () => {
+  const users = await prisma.user.findMany();
+  return users.map(user => ({
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    mobile: user.mobile,
+    createdAt: user.createdAt,
+  }));
+}
